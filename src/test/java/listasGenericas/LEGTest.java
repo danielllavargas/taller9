0ec5258f4ea5bf94473e32e054388bae2c48cb3b
danielavargas <danielllavargas@hotmail.com>
2013-08-10 07:53:20 -0500

    Taller 9 Listas Genericas

--- src/test/java/listasGenericas/LEGTest.java
+++ src/test/java/listasGenericas/LEGTest.java
@@ -20,6 +20,14 @@ public class LEGTest {
     @Test
     public void testInsertar() throws Exception {
 
+        LEG<Integer> i= new LEG<Integer>();
+
+        i.insertar(new Integer(9));
+        i.insertar(new Integer(10));
+        i.insertar(new Integer(11));
+        i.insertar(new Integer(12));
+
+        System.out.println("la lista es" + "\n" + i.toString());
     }
 
     @Test
@@ -34,6 +42,7 @@ public class LEGTest {
 
     @Test
     public void testToString() throws Exception {
+        LEG<Integer> i= new LEG<Integer>();
 
     }
 
